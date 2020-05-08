947b43c69ec70a3a78245ac187e2c6e0d44081f6
unknown <jethanhargrave@gmail.com>
2020-05-08 14:18:44 -0400

    fixed stupid little bug, off to lunch

--- data/projectRouter.js
+++ data/projectRouter.js
@@ -5,51 +5,92 @@ const Project = require('./helpers/projectModel')
 const router = express.Router();
 
 // get projects
-router.get('/', (req, res) => {
-    Project.get()
-        .then(item => {
-            res.status(200).json(item)
-        })
-        .catch(err => {
+router.get('/', (req, res) => {                                                   
+    Project.get()                                                                 
+        .then(item => {                                                           
+            res.status(200).json(item)                                            
+        })                                                                        
+        .catch(err => {                                                           
+            console.log(err)                                                      
+            res.status(500).json({ error: "Somethin' went wrong, bucko." })       
+        })                                                                        
+})
+
+// ----------------------------------------------------------------------------------------------------------------
+
+//get actions for a project                                                       
+router.get('/:id/actions', (req, res) => {                                        
+    const id = req.params.id;                                                      
+    Project.getProjectActions(id)                                                  
+        .then(item => {                                                            
+            res.status(200).json(item)                                             
+        })                                                                         
+        .catch(err => {                                                            
             console.log(err)
             res.status(500).json({ error: "Somethin' went wrong, bucko." })
         })
 })
 
-               // projects array: 
-
-               // [
-               //     {
-               //         "id": 1,
-               //         "name": "",
-               //         "description": "",
-               //         "completed": false
-               //     }
-               // ]
-
-//get actions for a project
-router.get('/:id/actions', (req, res) => {
-    const id = req.params.id;
-    Project.getProjectActions(id)
-        .then(item => {
-            res.status(200).json(item)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ error: "Somethin' went wrong, bucko." })
-        })
-})
+// ----------------------------------------------------------------------------------------------------------------
 
 
 //post a new project
 
 router.post('/', (req, res) => {
-    console.log(req.body)
+    const info = req.body;
+    Project.insert(info)
+      .then(item => {
+        res.status(201).json(item)
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "Somethin' went wrong, bucko." })
+      })
+  });
+
+// ----------------------------------------------------------------------------------------------------------------
+
+//get project by ID
+
+router.get('/:id', (req, res) => {
+    Project.get(req.params.id)
+    .then(item => {
+        res.status(200).json(item)
+    })
+    .catch(err =>
+        res.status(500).json({ error: "Somethin' went wrong, bucko" })
+    )
+});
+
+// ----------------------------------------------------------------------------------------------------------------
+
+//delete project
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    Project.remove(id)
+      .then(res.status(200).end())
+      .catch(err => {
+        res.status(500).json({ errorMessage: "Somethin' went wrong, bucko." })
+      })
 })
 
+// ----------------------------------------------------------------------------------------------------------------
 
+//update project
 
-
+router.put('/:id', (req, res) => {
+    console.log(req.body)
+    console.log(req.params)
+    const { id } = req.params;
+    const changes = req.body;
+    Project.update(id, changes)
+      .then(item => {
+          res.status(200).json(item)
+        })
+      .catch(err =>
+        res.send(500).json({ errorMessage: "Somethin' went wrong, bucko." })
+      );
+  });
 
 
 
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
 const projectRouter = require('./data/projectRouter.js')
+const actionRouter = require('./data/actionRouter.js')
 server.use('/api/projects', projectRouter)
 
-server.use(express.json());
+
 
 server.get('/', (req, res) => {
     res.send(`<h2> API </h2>`)
 })
+
 
 module.exports = server;
